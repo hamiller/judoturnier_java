ddf8a96a1d884fac4d67ab78ccbe96ec16a8b62c
Florian Miess <florian.miess@innoq.com>
2024-06-17 09:48:31 +0200

    Init

---
+++ src/main/java/de/sinnix/judoturnier/JudoturnierApplication.java
@@ -0,0 +1,13 @@
+package de.sinnix.judoturnier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudoturnierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudoturnierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/sinnix/judoturnier/JudoturnierApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sinnix.judoturnier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudoturnierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
