bfc1affab6f99d5597fa3d4b65936f88752fb941
Florian Miess <florian.miess@innoq.com>
2024-07-10 08:58:00 +0200

    Fixed test

--- src/test/java/de/sinnix/judoturnier/JudoturnierApplicationTests.java
+++ src/test/java/de/sinnix/judoturnier/JudoturnierApplicationTests.java
@@ -2,8 +2,10 @@ package de.sinnix.judoturnier;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class JudoturnierApplicationTests {
 
 	@Test
